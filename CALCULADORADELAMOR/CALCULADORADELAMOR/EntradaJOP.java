245310c23b171229ecce3dc792e61f54cc8771c3
KevMasMas <Kevindavidrincon01@gmail.com>
2025-03-27 19:46:55 -0500

    Delete CALCULADORADELAMOR/CALCULADORADELAMOR directory

--- CALCULADORADELAMOR/CALCULADORADELAMOR/EntradaJOP.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-// EntradaJOP.java (Entrada por JOptionPane)
-public class EntradaJOP extends Entrada {
-    @Override
-    public String pedirEntrada(String mensaje) {
-        return JOptionPane.showInputDialog(mensaje);
-    }
-}
--- CALCULADORADELAMOR/CALCULADORADELAMOR/Salida.java
+++
@@ -1,3 +0,0 @@
-public abstract class Salida {
-    public abstract void mostrarSalida(String mensaje);
-}
